ccfc9c16d19b37151f518d75554b2183aa914fa7
unknown <thitschler88@gmail.com>
2021-03-28 13:05:59 -0500

    Putting on the finishing touches for submission

--- script.js
+++ script.js
@@ -1,37 +1,33 @@
-// Write your JavaScript code here!
-
-/* This block of code shows how to format the HTML once you fetch some planetary JSON!
-<h2>Mission Destination</h2>
-<ol>
-   <li>Name: ${}</li>
-   <li>Diameter: ${}</li>
-   <li>Star: ${}</li>
-   <li>Distance from Earth: ${}</li>
-   <li>Number of Moons: ${}</li>
-</ol>
-<img src="${}">
-*/
-
-
-
-
-/**********
+/*********
  * 
- * TODO: Able to validate, now I need to update the Shuttle Stauts properly and give everything the green light once it's correct
+ * Assignment #5: Launch Checklist Form
+ * Create a Launch Checklist Form for astronauts to fill out in preparation for launch. The form should do the following:
+
+    -Use preventDefault() to prevent a request from being sent out and the page reloading.
+    -Validate the user-submitted data to ensure the following:
+        +The user entered something for every field.
+        +The user entered text for names and numbers for fuel and cargo levels.
+    -With validation, update a list of what is currently ready or not ready for the shuttle launch.
+    -Indicate what is good or bad about the shuttle and whether it is ready for launch by using the DOM to update the CSS.
+    -Fetch some planetary JSON to update the mission destination with vital facts and figures about where the shuttle is headed
  * 
- * ********/
+ * Author: Tom Hitschler
+ * Last edited: 3/28/21
+ * 
+ *********/
+
 window.addEventListener("load", function() //event listener looking for the window to load properly
 {
    console.log("window loaded");
    const form = document.getElementById("launchForm"); //grabbibng the reference to the HMTL form element
-   const pilotNameInput = document.querySelector("input[name=pilotName]");
+   const pilotNameInput = document.querySelector("input[name=pilotName]"); //grabbing inputs from text fields
    //console.log(pilotNameInput.value);
    const coPilotNameInput = document.querySelector("input[name=copilotName]");
    //console.log(coPilotNameInput.value);
    const fuelLevelInput = document.querySelector("input[name=fuelLevel]");
    const cargoMassInput = document.querySelector("input[name=cargoMass]");
    
-   function checkInputs(inputs) //check inputs values
+   function checkInputs(inputs) //function to check inputs values
    {
       //console.log(inputs);
 
@@ -40,37 +36,28 @@ window.addEventListener("load", function() //event listener looking for the wind
          alert("Invalid Input: Please enter a valid Pilot Name.");
          return false;        
       }
-      // else
-      // {
-      //    document.getElementById("pilotStatus").innerHTML = `Pilot ${inputs[i]} is ready for launch`;
-      // }
       else if(!isNaN(inputs[1]) || inputs[1]==='')
       {
          alert("Invalid Input: Please input a valid Copilot Name.");
          return false;
       }
-      // else
-      // {
-      //    document.getElementById("copilotStatus").innerHTML = `Co-pilot ${inputs[i]} is ready for launch`;
-      // }
       else if(isNaN(inputs[2]) || inputs[2]==='')
       {
          alert("Invalid Input: Please enter a number of (L) for Fuel Level.");
          return false;
       }
-      
       else if(isNaN(inputs[3]) || inputs[3]==='')
       {
          alert("Invalid Input: Please enter a number of (kg) for Cargo Mass.");
          return false;
       }
-      
-        
+
       return true;
    }
 
-   function statusCheckUpdate(inputs)
+   function statusCheckUpdate(inputs) //function to update status check based on inputs
    {
+      //update pilot stauts based on input
       if(inputs[0]==='')
       {
          document.getElementById("pilotStatus").innerHTML = `Pilot ${inputs[0]} is not ready for launch`;
@@ -79,69 +66,59 @@ window.addEventListener("load", function() //event listener looking for the wind
       {
          document.getElementById("pilotStatus").innerHTML = `Pilot ${inputs[0]} is ready for launch`;
       }
-      
+      //update copilot status based on input
       if(inputs[1]==='')
       {
          document.getElementById("copilotStatus").innerHTML = `Co-pilot ${inputs[1]} is not ready for launch`;
       }
-      
       else if(inputs[1])
       {
          document.getElementById("copilotStatus").innerHTML = `Co-pilot ${inputs[1]} is ready for launch`;
       }
-      
-      
-      
+      //update fuel level status based on input
       if(inputs[2]<10000)
       {
          document.getElementById("launchStatus").style.color ="red";
          document.getElementById("launchStatus").innerHTML = `Shuttle Not Ready For Launch`;
          document.getElementById("faultyItems").style.visibility='visible';
          document.getElementById("fuelStatus").innerHTML = `Fuel level too low for launch`;
-         //return false;
       }
       if (inputs[2]==='')
       {
          document.getElementById("fuelStatus").innerHTML = `Fuel level missing`;
       }
-      
+      //update cargo mass stauts based on input
       if(inputs[3]>10000)
       {
          document.getElementById("launchStatus").style.color ="red";
          document.getElementById("launchStatus").innerHTML = `Shuttle Not Ready For Launch`;
          document.getElementById("faultyItems").style.visibility='visible';
          document.getElementById("cargoStatus").innerHTML = `Cargo mass too high for launch`;
-         //return false;
       }
-      
       if(inputs[3] === '')
       {
          document.getElementById("cargoStatus").innerHTML = `Cargo mass missing`;
       }
-      
+      //return "Shuttle Ready for Launch" in green text and hide faulty items if inputs are correct
       if(inputs[2]>=10000 && inputs[3]<=10000 && inputs[2] !=='' && inputs[3] !=='')
       {
-         // document.getElementById("launchStatus").style.color ="red";
-         // document.getElementById("launchStatus").innerHTML = `Shuttle Not Ready For Launch`;
-         // document.getElementById("faultyItems").style.visibility='visible';
          document.getElementById("fuelStatus").innerHTML = `Fuel level high enough for launch`;
          document.getElementById("cargoStatus").innerHTML = `Cargo mass low enough for launch`;
          document.getElementById("launchStatus").style.color ="green";
          document.getElementById("launchStatus").innerHTML = `Shuttle Ready For Launch`;
          document.getElementById("faultyItems").style.visibility='hidden';
-         //return false;
       }  
    }
    
 
-   fetch("https://handlers.education.launchcode.org/static/planets.json").then(function(response)
+   fetch("https://handlers.education.launchcode.org/static/planets.json").then(function(response)//fetching JSON data for mission data
     {
         response.json().then( function(json) 
         {
             const missionTarget = document.getElementById("missionTarget");
             
             let div = document.createElement('div');  
-            const randIndex = Math.floor(Math.random() * json.length)
+            const randIndex = Math.floor(Math.random() * json.length)//grabs a random element in the array
             
             div.innerHTML = `
                <h2>Mission Destination</h2>
@@ -155,8 +132,6 @@ window.addEventListener("load", function() //event listener looking for the wind
                <img src="${json[randIndex].image}">
             </div>`;
             missionTarget.appendChild(div);
-            
-      
         });
    });
 
@@ -164,23 +139,18 @@ window.addEventListener("load", function() //event listener looking for the wind
    {
       
       event.preventDefault();//prevents form submission
-      let inputs = [];
-      const pilotNameValue = pilotNameInput.value.trim();
+      let inputs = []; //declare arry to hold inputs
+      const pilotNameValue = pilotNameInput.value.trim();//trim any white space around entry
       //console.log(pilotNameValue);
-      inputs.push(pilotNameValue);
+      inputs.push(pilotNameValue);//push value into the array
       const coPilotNameValue = coPilotNameInput.value.trim();
       inputs.push(coPilotNameValue);
       const fuelLevelValue = fuelLevelInput.value.trim();
       inputs.push(fuelLevelValue);
       const cargoMassValue = cargoMassInput.value.trim();
       inputs.push(cargoMassValue);
+
       checkInputs(inputs);
       statusCheckUpdate(inputs);
-      
-      // if(checkInputs(inputs));
-      // {
-      //    statusCheckUpdate(inputs);
-      // }
-
    });
 });
